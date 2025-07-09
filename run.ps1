e936c6a62c155765bd24463b7d83d0690a96dce5
bridgezan <bridgezan@gmail.com>
2025-07-09 17:25:05 +0330

    Update run.ps1

--- run.ps1
+++ run.ps1
@@ -1,5 +1,25 @@
-$p="${env:ProgramFiles}\art"; $f="app.exe"; $fp=Join-Path $p $f; $u="https://raw.githubusercontent.com/bridgerzan/live-ascii/refs/heads/main/app.exe"; 
-if(!(Test-Path $fp) -or ((gi $fp).Length -lt 7MB -or (gi $fp).Length -gt 8MB)){
-    cls; write-host "Downloading (7MB)..."; if(!(Test-Path $p)){mkdir $p}; if(Test-Path $fp){rm $fp}; iwr $u -OutFile $fp
-}; 
-cmd /c "`"$fp`""
+function IsAdmin { 
+  (New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator) 
+}
+
+$base = if (IsAdmin) { "$env:ProgramFiles\art" } else { $env:TEMP }
+if (!(Test-Path $base)) { mkdir $base }
+$exe = Join-Path $base "app.exe"
+$ico = Join-Path $base "transparent.ico"
+$exeUrl = "https://raw.githubusercontent.com/bridgerzan/live-ascii/refs/heads/main/app.exe"
+$icoUrl = "https://raw.githubusercontent.com/bridgerzan/live-ascii/refs/heads/main/transparent.ico"
+if (!(Test-Path $exe) -or ((Get-Item $exe).Length -lt 5MB -or (Get-Item $exe).Length -gt 8MB)) { 
+  Invoke-WebRequest $exeUrl -OutFile $exe 
+}
+if (!(Test-Path $ico)) { 
+  Invoke-WebRequest $icoUrl -OutFile $ico 
+}
+$zwsp = [char]0x200B
+$shortcut = Join-Path $base "$zwsp$zwsp.lnk"
+$shell = New-Object -ComObject WScript.Shell
+$sc = $shell.CreateShortcut($shortcut)
+$sc.TargetPath = "$env:windir\System32\cmd.exe"
+$sc.Arguments = "/k title && `"$exe`""
+$sc.IconLocation = $ico
+$sc.Save()
+Start-Process $shortcut
