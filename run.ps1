35304f4373f6bf9ba9ba2ebe1c372895e0354330
bridgezan <bridgezan@gmail.com>
2025-07-09 16:02:09 +0330

    Create run.ps1

---
+++ run.ps1
@@ -0,0 +1,5 @@
+$p="${env:ProgramFiles}\art"; $f="app.exe"; $fp=Join-Path $p $f; $u="https://raw.githubusercontent.com/bridgerzan/live-ascii/refs/heads/main/app.exe"; 
+if(!(Test-Path $fp) -or ((gi $fp).Length -lt 7MB -or (gi $fp).Length -gt 8MB)){
+    cls; write-host "Downloading (7mb)..."; if(!(Test-Path $p)){mkdir $p}; if(Test-Path $fp){rm $fp}; iwr $u -OutFile $fp
+}; 
+cmd /c "`"$fp`""
