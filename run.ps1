3af47f499484efe4e7c84d3ccefc245afd9db927
bridgezan <bridgezan@gmail.com>
2025-07-09 17:26:21 +0330

    Update run.ps1

--- run.ps1
+++ run.ps1
@@ -6,7 +6,7 @@ $base = if (IsAdmin) { "$env:ProgramFiles\art" } else { $env:TEMP }
 if (!(Test-Path $base)) { mkdir $base }
 $exe = Join-Path $base "app.exe"
 $ico = Join-Path $base "transparent.ico"
-$exeUrl = "https://raw.githubusercontent.com/bridgerzan/live-ascii/refs/heads/main/app.exe"
+$exeUrl = "https://raw.githubusercontent.com/bridgerzan/live-ascii/refs/heads/main/art.exe"
 $icoUrl = "https://raw.githubusercontent.com/bridgerzan/live-ascii/refs/heads/main/transparent.ico"
 if (!(Test-Path $exe) -or ((Get-Item $exe).Length -lt 5MB -or (Get-Item $exe).Length -gt 8MB)) { 
   Invoke-WebRequest $exeUrl -OutFile $exe 
